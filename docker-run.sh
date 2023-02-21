9ae9691f4cf10d8da1bf48458eea8858806db366
adrianodonisete <adrianodonisete@gmail.com>
2023-02-21 15:40:53 -0300

    adding notebooks

---
+++ docker-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run -i -t -p 8888:8888 continuumio/miniconda3 /bin/bash -c "\
+    conda install jupyter -y --quiet && \
+    mkdir -p /opt/notebooks && \
+    jupyter notebook \
+    --notebook-dir=/opt/notebooks --ip='*' --port=8888 \
+    --no-browser --allow-root"
