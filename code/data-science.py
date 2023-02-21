60fbcec4051b8dd9c90a9b8c2aa5ac04af31ba34
adrianodonisete <adrianodonisete@gmail.com>
2023-02-21 15:40:34 -0300

    start project

---
+++ code/data-science.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ("Hello, World!")
+
+
+# In[2]:
+
+
+print ("estou aprendendo pythou")
+
+
+# In[3]:
+
+
+1+1
+
+
+# In[4]:
+
+
+a = 4
+
+
+# In[5]:
+
+
+b = 6
+
+
+# In[6]:
+
+
+a*b
+
+
+# In[7]:
+
+
+a.upper()
+
+
+# In[8]:
+
+
+tx = "vai"
+
+
+# In[9]:
+
+
+tx.upper()
+
+
+# In[ ]:
+
+
+
+
